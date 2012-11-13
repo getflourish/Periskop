e65f8032d2602fe5326f8abbbd076f7f0a79ff49
Julian Stahnke <mail@julianstahnke.com>
2012-11-13 18:55:34 +0100

    Transition

--- periskop.js
+++ periskop.js
@@ -1,5 +1,6 @@
 window.onload = function() {
-  var Periskop = {
+  window.Periskop = {
+      images: [],
       init: function () {
           if (navigator.geolocation) {
               navigator.geolocation.watchPosition(Periskop.update)
@@ -17,7 +18,48 @@ window.onload = function() {
           fov = typeof fov !== 'undefined' ? fov : 90;
           heading = typeof heading !== 'undefined' ? heading : 0;
           pitch = typeof pitch !== 'undefined' ? pitch : 0;
-          $("#streetview").html("<img src='http://maps.googleapis.com/maps/api/streetview?size=640x640&location="+ latitude + ",%20" + longitude + "&fov=" + fov + "&heading=" + heading + "&pitch=" + pitch + "&sensor=true'>");
+          var image = new Image();
+          image.id = "t"+Periskop.getTimeStamp();
+          Periskop.images.push(image);
+          image.onload = function (event) {
+              $(image).data("loaded", true);
+              Periskop.showLatestImage();
+          };
+          image.src = "http://maps.googleapis.com/maps/api/streetview?size=640x640&location="+ latitude + ",%20" + longitude + "&fov=" + fov + "&heading=" + heading + "&pitch=" + pitch + "&sensor=true";
+      },
+      showLatestImage: function () {
+          var done = false;
+          for (var i = Periskop.images.length - 1; i >= 0; i--) {
+              if ($(Periskop.images[i]).data("loaded")) {
+                  done = i;
+                  break;
+              }
+          };
+          if (done) {
+              $("#streetview").append(Periskop.images[done]);
+              // is there an old image that needs removing?
+              if ($("#streetview").children().size() > 1) {
+                  var oldImage = $("#streetview").children().first();
+                  console.dir(oldImage);
+                  oldImage.animate(
+                      {
+                          opacity: 0,
+                          scale: 2
+                      },
+                      {
+                          complete: function (event) {
+                              // alle vorigen Bilder löschen
+                              oldImage.remove();
+                              Periskop.images.splice(0, i+1);
+                          }
+                      }
+                  );
+              } else {
+                  // no old images, but our queue needs to be cleaned out,
+                  // delete all images we tried to load previously
+                  Periskop.images.splice(0, i+1);
+              }
+          }
       },
       update: function (position) {
           position = position.coords;
